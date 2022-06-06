3e4a309d54ee6d6698fd9513874eeada2c0ed5fa
David Koifman <koifmanDavid123@gmail.com>
2022-06-06 21:12:57 +0600

    Intial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Work on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
