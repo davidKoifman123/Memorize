195815ed909da52602656901056ab6e4d2f777f0
David Koifman <koifmanDavid123@gmail.com>
2022-06-07 23:42:02 +0600

    Create CardView struct

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Card")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+  //  var isFaceUp: Bool { return false }
+    
+    var body: some View {
+        ZStack {
+            //if isFaceUp {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill()
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3)
+                Text("🎾")
+                    .font(.largeTitle)
+          //  } else {
+             //   RoundedRectangle(cornerRadius: 20)
+                 //   .fill()
+            //}
+        }
     }
 }
 
