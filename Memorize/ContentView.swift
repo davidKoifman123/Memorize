ea248c7176bcd21af9f1ccc6ae9031ccc319133f
David Koifman <koifmanDavid123@gmail.com>
2022-06-11 00:51:19 +0600

    Remove plus/minus buttons

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -33,28 +33,7 @@ struct ContentView: View {
             .padding(.horizontal)
     }
         .padding(.horizontal)
-}
-    
-    var remove: some View {
-        Button(action: {
-            if emojiCount > 1 {
-            emojiCount -= 1
-            }
-        }, label: {
-            Image(systemName: "minus.circle")
-        })
-    }
-               
-               
-    var add: some View {
-        Button(action: {
-            if emojiCount < emojis.count {
-             emojiCount += 1
-            }
-            }, label: {
-               Image(systemName: "plus.circle")
-             })
-        }
+ }
 }
 
 struct CardView: View {
