8ce434e7ffd978245e9b430d4257712beb0bd56b
David Koifman <koifmanDavid123@gmail.com>
2022-06-11 23:39:32 +0600

    Create MemoryGameStruct

--- MemoryGame.swift
+++ MemoryGame.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMathched: Bool
+        var content: CardContent
+    }
+}
