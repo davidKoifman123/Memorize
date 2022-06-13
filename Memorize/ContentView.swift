9a7c0c837bd14bfc7453523fa2b9aa9a1ac1e5aa
David Koifman <koifmanDavid123@gmail.com>
2022-06-13 18:21:43 +0600

    Improve Logic of the Game

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,18 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    //TODO: deal with redudncay
-   @State var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
-    
-    @State var emojiCount = 20
+    let viewModel: EmojiMemoryGame
     
     var body: some View {
         VStack {
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    ForEach(viewModel.cards) { card in
+                    CardView(card: card).aspectRatio(2/3, contentMode: .fit)
                 }
             }
       }
@@ -30,29 +26,26 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var content: String
-   @State var isFaceUp: Bool = true
-
-    
+    let card: MemoryGame<String>.Card
+   
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
-            if isFaceUp {
+            if card.isFaceUp {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
+                Text(card.content).font(.largeTitle)
             } else {
                 shape.fill()
             }
-        }
-        .onTapGesture {
-            isFaceUp = !isFaceUp
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let game = EmojiMemoryGame()
+        ContentView(viewModel: game).preferredColorScheme(.dark)
+        ContentView(viewModel: game).preferredColorScheme(.light)
     }
 }
--- MemoryGame.swift
+++ MemoryGame.swift
@@ -19,14 +19,15 @@ struct MemoryGame<CardContent> {
         
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = createCardContent(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: pairIndex * 2))
+            cards.append(Card(content: content, id: pairIndex * 2 + 1))
         }
     }
     
-    struct Card {
-        var isFaceUp: Bool = false
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
         var isMathched: Bool = false
         var content: CardContent
+        var id: Int
     }
 }
