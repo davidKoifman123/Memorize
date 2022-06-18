caea2c17385f8964e3660581acf33a7ba93f68ef
David Koifman <koifmanDavid123@gmail.com>
2022-06-18 21:29:09 +0600

    Add Rotation Animation

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -35,22 +35,26 @@ struct CardView: View {
             ZStack {
                     Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 110 - 90))
                         .padding(5).opacity(0.5)
-                    Text(card.content).font(font(in: geometry.size))
+                Text(card.content).rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
     }
     
-    private func font(in size: CGSize) -> Font {
-        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
-    }
+   private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
+}
 
-private struct DrawingConstants {
+  private struct DrawingConstants {
     static let fontScale: CGFloat = 0.7
+    static let fontSize: CGFloat = 32
    }
 }
 
-struct ContentView_Previews: PreviewProvider {
+  struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
         game.choose(game.cards.first!)
