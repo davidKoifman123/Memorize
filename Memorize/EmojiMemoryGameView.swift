e15bb4c8fe61857461e7a3e4305cddcdc8076e29
David Koifman <koifmanDavid123@gmail.com>
2022-06-20 01:04:04 +0600

    Add Remaining Time Animation

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -118,19 +118,34 @@ struct EmojiMemoryGameView: View {
 
 struct CardView: View {
     let card: EmojiMemoryGame.Card
+    
+    @State private var animatedBonusRemaining: Double = 0
 
-    var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                    Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 110 - 90))
-                        .padding(5).opacity(0.5)
-                Text(card.content).rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
-                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
-                    .font(Font.system(size: DrawingConstants.fontSize))
-                    .scaleEffect(scale(thatFits: geometry.size))
+  var body: some View {
+    GeometryReader { geometry in
+      ZStack {
+        Group {
+          if card.isConsumingBonusTime {
+            Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: (1-animatedBonusRemaining) * 360 - 90))
+              .onAppear {
+                animatedBonusRemaining = card.bonusRemaining
+                withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+                    animatedBonusRemaining = 0
+                }
+              }
+            } else {
+                Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: (1-card.bonusRemaining) * 360 - 90))
             }
-            .cardify(isFaceUp: card.isFaceUp)
+         }
+            .padding(5)
+            .opacity(0.5)
+         Text(card.content).rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+            .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+            .font(Font.system(size: DrawingConstants.fontSize))
+            .scaleEffect(scale(thatFits: geometry.size))
         }
+        .cardify(isFaceUp: card.isFaceUp)
+      }
     }
     
    private func scale(thatFits size: CGSize) -> CGFloat {
