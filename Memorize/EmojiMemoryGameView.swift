05bc77d42d60f8fd58acc195c633ad9cbc4c4b40
David Koifman <koifmanDavid123@gmail.com>
2022-06-19 15:59:13 +0600

    Add Cards Flipping Animation

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,19 +11,36 @@ struct EmojiMemoryGameView: View {
    @ObservedObject var game: EmojiMemoryGame
     
     var body: some View {
+        VStack {
+            gameBody
+            shuffle
+        }
+        .padding()
+    }
+    
+    var gameBody: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
             if card.isMatched && !card.isFaceUp {
-                Rectangle().opacity(0)
+                Color.clear
             } else {
             CardView(card: card)
                 .padding(4)
                 .onTapGesture {
+                    withAnimation(.easeInOut(duration: 3)) {
                     game.choose(card)
+                    }
                  }
                }
         }
         .foregroundColor(.red)
-        .padding(.horizontal)
+    }
+    
+    var shuffle: some View {
+        Button("Shuffle") {
+            withAnimation {
+            game.shuffle()
+            }
+        }
     }
 }
 
